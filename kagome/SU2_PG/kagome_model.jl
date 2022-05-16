9a82d9cf269f3803e248f092bd49737465152401
niusen <1501110187@pku.edu.cn>
2022-05-16 23:32:06 +0200

    Update kagome_model.jl

--- kagome/SU2_PG/kagome_model.jl
+++ kagome/SU2_PG/kagome_model.jl
@@ -17,6 +17,7 @@ function evaluate_ob(parameters, U_phy, A_unfused, A_fused, AA_fused, U_L,U_D,U_
         E_up=E_up[1];
 
         rho_LU_RU_LD=ob_LU_RU_LD(CTM,AA_fused,AA2,AA1,AA3);
+        rho_LU_RU_LD=permute(rho_LU_RU_LD,(1,3,2,),());#anti-clock-wise order
         @tensor rho_LU_RU_LD[:]:=U_ss[-1,-4,1]*U_ss[-2,-5,2]*U_ss[-3,-6,3]*rho_LU_RU_LD[1,2,3];
         @tensor rho_LU_RU_LD[:]:=U_phy'[4,5,6,-1]*rho_LU_RU_LD[4,5,6,1,2,3]*U_phy[-2,1,2,3];
         #@tensor e[:]:=rho_LU_RU_LD[1,2]*H_triangle[2,1];
@@ -79,16 +80,19 @@ function Hamiltonians(U_phy,J1,J2,J3,Jchi,Jtrip)
     @tensor H12[:]:=sx[-1,-4]*sx[-2,-5]*Id[-3,-6]+sy[-1,-4]*sy[-2,-5]*Id[-3,-6]+sz[-1,-4]*sz[-2,-5]*Id[-3,-6];
     @tensor H31[:]:=sx[-1,-4]*Id[-2,-5]*sx[-3,-6]+sy[-1,-4]*Id[-2,-5]*sy[-3,-6]+sz[-1,-4]*Id[-2,-5]*sz[-3,-6];
     @tensor H23[:]:=Id[-1,-4]*sx[-2,-5]*sx[-3,-6]+Id[-1,-4]*sy[-2,-5]*sy[-3,-6]+Id[-1,-4]*sz[-2,-5]*sz[-3,-6];
+    @tensor H123chiral[:]:=sx[-1,-4]*sy[-2,-5]*sz[-3,-6]-sx[-1,-4]*sz[-2,-5]*sy[-3,-6]+sy[-1,-4]*sz[-2,-5]*sx[-3,-6]-sy[-1,-4]*sx[-2,-5]*sz[-3,-6]+sz[-1,-4]*sx[-2,-5]*sy[-3,-6]-sz[-1,-4]*sy[-2,-5]*sx[-3,-6];
     H12_tensorkit=J1*TensorMap(H12, domain(U_phy) ← domain(U_phy));
     H31_tensorkit=J1*TensorMap(H31, domain(U_phy) ← domain(U_phy));
     H23_tensorkit=J1*TensorMap(H23, domain(U_phy) ← domain(U_phy));
+    H123chiral_tensorkit=Jtrip*TensorMap(H123chiral, domain(U_phy) ← domain(U_phy));
     @tensor H12_tensorkit[:]:=U_phy'[4,5,6,-1]*H12_tensorkit[1,2,3,4,5,6]*U_phy[-2,1,2,3];
     @tensor H31_tensorkit[:]:=U_phy'[4,5,6,-1]*H31_tensorkit[1,2,3,4,5,6]*U_phy[-2,1,2,3];
     @tensor H23_tensorkit[:]:=U_phy'[4,5,6,-1]*H23_tensorkit[1,2,3,4,5,6]*U_phy[-2,1,2,3];
+    @tensor H123chiral_tensorkit[:]:=U_phy'[4,5,6,-1]*H123chiral_tensorkit[1,2,3,4,5,6]*U_phy[-2,1,2,3];
 
     @tensor H_Heisenberg[:]:=sx[-1,-3]*sx[-2,-4]+sy[-1,-3]*sy[-2,-4]+sz[-1,-3]*sz[-2,-4];
 
-    H_triangle=H12_tensorkit+H31_tensorkit+H23_tensorkit;
+    H_triangle=H12_tensorkit+H31_tensorkit+H23_tensorkit+H123chiral_tensorkit;
     H_bond=J1*H_Heisenberg;
     return H_triangle, H_bond, H12_tensorkit, H31_tensorkit, H23_tensorkit 
 end

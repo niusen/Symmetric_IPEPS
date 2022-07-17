db808b1698840dfbba7bc5377be1135fcfabfeb4
niusen <1501110187@pku.edu.cn>
2022-07-17 18:50:47 +0200

    Update test_ES_ED.jl

--- kagome/SU2_PG/test_ES_ED.jl
+++ kagome/SU2_PG/test_ES_ED.jl
@@ -19,9 +19,9 @@ include("mps_algorithms/ES_preliminary.jl")
 
 
 D=8;
-chi=20;
-N=4;
-EH_n=60;#number of entanglement spectrum
+chi=30;
+N=6;
+EH_n=10;#number of entanglement spectrum
 filenm="julia_LS_D_8_chi_40.json"
 
 J1=0.80902;
@@ -31,8 +31,8 @@ Jchi=0;
 Jtrip=0.5878;
 parameters=Dict([("J1", J1), ("J2", J2), ("J3", J3), ("Jchi", Jchi), ("Jtrip", Jtrip)]);
 
-CTM_conv_tol=1e-6;    
-CTM_ite_nums=50;
+CTM_conv_tol=1e-6;
+CTM_ite_nums=200;
 CTM_trun_tol=1e-12;
-
-ES_CTMRG_ED(filenm,parameters,D,chi,N,EH_n,CTM_conv_tol,CTM_ite_nums,CTM_trun_tol)
+group_index=true;
+ES_CTMRG_ED_Kprojector(filenm,parameters,D,chi,N,EH_n,CTM_conv_tol,CTM_ite_nums,CTM_trun_tol,group_index)

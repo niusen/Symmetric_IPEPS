eec87cd5434fbd8ee556864df54b0aa3ffbaf3ec
niusen <1501110187@pku.edu.cn>
2022-11-21 11:15:04 +0100

    Create SU_9up_9down.jl

---
+++ kagome/simple_update/SU_9up_9down.jl
@@ -0,0 +1,85 @@
+using LinearAlgebra
+using TensorKit
+using KrylovKit
+using JSON
+using HDF5, JLD
+using Random
+cd("D:\\My Documents\\Code\\Julia_codes\\Tensor network\\IPEPS_TensorKit\\kagome\\simple_update")
+#push!(LOAD_PATH, "D:\\My Documents\\Code\\Julia_codes\\Tensor network\\IPEPS_TensorKit\\kagome\\SU2_PG")
+include("D:\\My Documents\\Code\\Julia_codes\\Tensor network\\IPEPS_TensorKit\\kagome\\simple_update\\resource_codes\\kagome_load_tensor.jl")
+include("D:\\My Documents\\Code\\Julia_codes\\Tensor network\\IPEPS_TensorKit\\kagome\\simple_update\\resource_codes\\kagome_CTMRG.jl")
+include("D:\\My Documents\\Code\\Julia_codes\\Tensor network\\IPEPS_TensorKit\\kagome\\simple_update\\resource_codes\\kagome_model.jl")
+include("D:\\My Documents\\Code\\Julia_codes\\Tensor network\\IPEPS_TensorKit\\kagome\\simple_update\\resource_codes\\kagome_IPESS.jl")
+include("D:\\My Documents\\Code\\Julia_codes\\Tensor network\\IPEPS_TensorKit\\kagome\\simple_update\\resource_codes\\kagome_FiniteDiff.jl")
+
+include("funs_9up_9down.jl")
+
+
+Random.seed!(1234)
+
+D_max=25;
+
+trun_tol=1e-6;
+D=3;
+
+
+theta=0*pi;
+J1=cos(theta);
+J2=0;
+J3=0;
+Jchi=0;
+Jtrip=sin(theta);
+
+parameters=Dict([("J1", J1), ("J2", J2), ("J3", J3), ("Jchi", Jchi), ("Jtrip", Jtrip)]);
+
+
+
+#state_dict=read_json_state("LS_D_8_chi_40.json")
+init_statenm=nothing;
+#init_statenm="julia_LS_D_8_chi_40.json"
+init_noise=0;
+Bond_irrep="A";
+Triangle_irrep="A1+iA2";
+nonchiral="A1_even";
+#nonchiral="No"
+
+A_set,B_set,A1_set,A2_set, A_set_occu,B_set_occu,A1_set_occu,A2_set_occu, S_label, Sz_label, virtual_particle, Va, Vb=construct_tensor(D);
+state_dict, Bond_A_coe, Bond_B_coe, Triangle_A1_coe, Triangle_A2_coe, A1_has_odd, A2_has_odd=initial_state(Bond_irrep,Triangle_irrep,nonchiral,D,init_statenm,init_noise);
+bond_tensor,triangle_tensor=construct_su2_PG_IPESS(state_dict,A_set,B_set,A1_set,A2_set, A_set_occu,B_set_occu,A1_set_occu,A2_set_occu, S_label, Sz_label, virtual_particle, Va, Vb);
+
+T_u=deepcopy(triangle_tensor);
+T_d=deepcopy(triangle_tensor);
+B_a=deepcopy(bond_tensor);
+B_b=deepcopy(bond_tensor);
+B_c=deepcopy(bond_tensor);
+lambda_u_a=unitary(space(bond_tensor,1),space(bond_tensor,1));
+lambda_u_a=lambda_u_a'*lambda_u_a;
+lambda_u_b=deepcopy(lambda_u_a);
+lambda_u_c=deepcopy(lambda_u_a);
+lambda_d_a=deepcopy(lambda_u_a);
+lambda_d_b=deepcopy(lambda_u_a);
+lambda_d_c=deepcopy(lambda_u_a);
+
+
+U_d=space(bond_tensor,3);
+U_phy_3=unitary(fuse(U_d ⊗ U_d ⊗ U_d), U_d ⊗ U_d ⊗ U_d);
+U_phy_2=unitary(fuse(U_d ⊗ U_d), U_d ⊗ U_d);
+H_triangle, H_Heisenberg, H12_tensorkit, H31_tensorkit, H23_tensorkit=Hamiltonians(U_phy_3,parameters["J1"],parameters["J2"],parameters["J3"],parameters["Jchi"],parameters["Jtrip"])
+@tensor H_triangle[:]:=U_phy_3[2,-1,-2,-3]*H_triangle[2,1]*U_phy_3'[-4,-5,-6,1];
+H_triangle=permute(H_triangle,(1,2,3,),(4,5,6,));
+H_Heisenberg=TensorMap(H_Heisenberg, U_d' ⊗ U_d' ← U_d' ⊗ U_d');
+
+tau=5;
+dt=0.02;
+
+T_u,T_d,B_a,B_b,B_c,lambda_u_a,lambda_u_b,lambda_u_c,lambda_d_a,lambda_d_b,lambda_d_c=itebd(T_u,T_d,B_a,B_b,B_c,lambda_u_a,lambda_u_b,lambda_u_c,lambda_d_a,lambda_d_b,lambda_d_c, H_triangle, trun_tol, tau, dt, D_max);
+
+
+
+println(space(T_u))
+println(space(T_d))
+
+
+
+
+

36ca822420f401fc1061c91a7a6394017bde8fdd
niusen <1501110187@pku.edu.cn>
2023-09-12 22:05:14 +0200

    Update pyrochlore_load_tensor.jl

--- pyrochlore/2D/pyrochlore_load_tensor.jl
+++ pyrochlore/2D/pyrochlore_load_tensor.jl
@@ -46,7 +46,7 @@ function construct_tensor(D)
         
         t1 = TensorMap(T, V1 ⊗ V2  ← V3)
 
-        Va=SU2Space(0=>length(findall(x->x==0, S_label))/1, 1/2=>length(findall(x->x==1/2, S_label))/2,1=>length(findall(x->x==1, S_label))/3,3/2=>length(findall(x->x==3/2, S_label))/4)
+        Va=SU2Space(0=>length(findall(x->x==0, S_label))/1, 1/2=>length(findall(x->x==1/2, S_label))/2,1=>length(findall(x->x==1, S_label))/3,3/2=>length(findall(x->x==3/2, S_label))/4,2=>length(findall(x->x==2, S_label))/5,5/2=>length(findall(x->x==5/2, S_label))/6)
         Vb=SU2Space(1=>1)
         t2 = TensorMap(T, Va ⊗ Va ← Vb);
         #print(convert(Array, t2))

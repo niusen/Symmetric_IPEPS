d04531c9f4eb16305b2155e52baac24b2d92d082
niusen <1501110187@pku.edu.cn>
2023-10-05 18:44:42 +0200

    Update spin_operator.jl

--- pyrochlore/2D/spin_operator.jl
+++ pyrochlore/2D/spin_operator.jl
@@ -37,6 +37,25 @@ function plaquatte_Heisenberg(J1,J2)
 
 end
 
+function plaquatte_coupling()
+    SS_op12=spin1_op();
+    SS_op13=permute(SS_op12,(1,3,2,4,),(5,7,6,8,));
+    SS_op14=permute(SS_op12,(1,4,3,2,),(5,8,7,6,));
+    SS_op23=permute(SS_op12,(3,2,1,4,),(7,6,5,8,));
+    SS_op24=permute(SS_op12,(4,2,3,1,),(8,6,7,5,));
+    SS_op34=permute(SS_op12,(3,4,1,2,),(7,8,5,6,));
+
+    check_hermitian(SS_op13);
+    check_hermitian(SS_op14);
+    check_hermitian(SS_op23);
+    check_hermitian(SS_op24);
+    check_hermitian(SS_op34);
+
+    
+    return SS_op12, SS_op13, SS_op14, SS_op23, SS_op24, SS_op34
+
+end
+
 function plaquatte_AKLT(Sigma)
     AKLT=Sigma+Sigma*Sigma*8/19+Sigma*Sigma*Sigma/19+12/19*unitary(domain(Sigma),domain(Sigma));
     return AKLT

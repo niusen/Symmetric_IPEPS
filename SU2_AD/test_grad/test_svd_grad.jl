e30052abf148303cbe5b2051c343eb6548855fae
unknown <1501110187@pku.edu.cn>
2025-07-31 22:27:00 -0700

    Update test_svd_grad.jl

--- SU2_AD/test_grad/test_svd_grad.jl
+++ SU2_AD/test_grad/test_svd_grad.jl
@@ -21,7 +21,7 @@ include("../src/bosonic/optimkit_lib.jl")
 include("../src/bosonic/CTMRG.jl")
 include("../src/fermionic/Fermionic_CTMRG.jl")
 include("../src/fermionic/Fermionic_CTMRG_unitcell.jl")
-include("../src/fermionic/Fermionic_CTMRG_unitcell_iPESS.jl")
+# include("../src/fermionic/Fermionic_CTMRG_unitcell_iPESS.jl")
 include("../src/fermionic/square_Hubbard_model_cell.jl")
 include("../src/fermionic/swap_funs.jl")
 include("../src/fermionic/mpo_mps_funs.jl")
@@ -41,7 +41,7 @@ projector_trun_tol=1e-8;
 
 
 
-data=load("stochastic_iPESS_LS_D_8_chi_80.jld2");
+data=load("stochastic_iPESS_LS_D_8_chi_80_2.3399.jld2");
 
 
 
@@ -74,7 +74,7 @@ AA, U_L,U_D,U_R,U_U=build_double_layer_swap(A_set[pos[1],pos[2]]',A_set[pos[1],p
 chi=30;
 function cfun1(M)
     M=permute(M,(1,2,),(3,4,));
-    uM,sM,vM = my_tsvd(M; trunc=truncdim(chi));
+    uM,sM,vM = tsvd(M; trunc=truncdim(chi));
     sM_inv_sqrt=sdiag_inv_sqrt(sM);
 
     mm=uM*sM_inv_sqrt*vM;
@@ -100,3 +100,35 @@ y2=cfun2(AA);
 ∂E2=gradient(x ->cfun2(x), AA)[1];
 
 dot(∂E1,∂E2)/sqrt(dot(∂E1,∂E1)*dot(∂E2,∂E2))
+
+#check multiplet truncation
+
+M=permute(AA,(1,2,),(3,4,));
+uM0,sM0,vM0 = tsvd(M; trunc=truncdim(chi+10));
+uM1,sM1,vM1 = tsvd(M; trunc=truncdim(chi));
+uM2,sM2,vM2 = tsvd(M; trunc=truncdim(chi; multiplet_tol=0.018));
+println(space(sM2))
+for (a,b) in blocks(sM0)
+    println(a)
+    if a==Irrep[SU₂](0)
+        println(a,diag(b)[1:16])
+    end
+end
+
+
+data=load("test.jld2");
+M=data["M"];
+chi_=data["chi_"];
+uM_,sM_,vM_ = tsvd(M; trunc=truncdim(chi_));
+println(space(sM_))
+uM2_,sM2_,vM2_ = tsvd(M; trunc=truncdim(chi_; multiplet_tol=1e-5));
+println(space(sM2_))
+uM2_,sM2_,vM2_ = tsvd(M; trunc=truncdim(chi_+3; multiplet_tol=1e-5));
+println(space(sM2_))
+uM_,sM_,vM_ = tsvd(M);
+for (a,b) in blocks(sM2_)
+    println(a)
+    if a==Irrep[SU₂](1/2)
+        println(a,diag(b)[:])
+    end
+end
